4208f9209af0fbabfaf8aec6e47a6ccca3e8bbe3
Poonam Tiwari <poonam.tiwari@coditas.com>
2020-02-03 15:56:35 +0530

    sdfv

--- dist/all/@Orxe/Orxe/culture/culture.js
+++ dist/all/@Orxe/Orxe/culture/culture.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = require("@Orxe/http");
+const http_1 = require("@ptiwari/erox-sdk/Orxe/http");
 class Culture {
     getHttpMethod() {
         http_1.Http.sampleMethod();
